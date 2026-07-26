7d90742cbb3eab62cd2ffb45d277279d9fee029f
asmi046 <asmi046@gmail.com>
2026-07-27 00:12:51 +0300

    ссылка на max

--- resources/views/components/price.blade.php
+++ resources/views/components/price.blade.php
@@ -1,9 +1,9 @@
 @php
     $packages = [
         [
-            'image'    => asset('images/tarif/tarif_1.webp'),
-            'name'     => 'Стандартный',
-            'price'    => '100 000 ₽',
+            'image' => asset('images/tarif/tarif_1.webp'),
+            'name' => 'Стандартный',
+            'price' => '100 000 ₽',
             'featured' => false,
             'features' => [
                 'Ведение свадьбы с 17 до 22.',
@@ -14,9 +14,9 @@
             ],
         ],
         [
-            'image'    => asset('images/tarif/tarif_2.webp'),
-            'name'     => 'Комфорт',
-            'price'    => '130 000 ₽',
+            'image' => asset('images/tarif/tarif_2.webp'),
+            'name' => 'Комфорт',
+            'price' => '130 000 ₽',
             'featured' => true,
             'features' => [
                 'Ведение свадьбы с 15 до 23 часов: welcome, церемония и банкет.',
@@ -27,9 +27,9 @@
             ],
         ],
         [
-            'image'    => asset('images/tarif/tarif_3.webp'),
-            'name'     => 'VIP',
-            'price'    => '150 000 ₽',
+            'image' => asset('images/tarif/tarif_3.webp'),
+            'name' => 'VIP',
+            'price' => '150 000 ₽',
             'featured' => false,
             'features' => [
                 'Ведение свадьбы с 15 до 23 часов: welcome, церемония и банкет.',
@@ -64,14 +64,17 @@
                 @foreach ($package['features'] as $feature)
                     <li>
                         <span class="ico ico--ok">
-                            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3"><polyline points="20 6 9 17 4 12"/></svg>
+                            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3">
+                                <polyline points="20 6 9 17 4 12" />
+                            </svg>
                         </span>
                         {{ $feature }}
                     </li>
                 @endforeach
             </ul>
 
-            <a href="#" class="card__btn">Заказать</a>
+            <a href="https://max.ru/u/f9LHodD0cOLLrUIoYsnlgwycVyYR0OWgfQVSlFfqP-34-sYdK0vMq40pVvE"
+                class="card__btn">Заказать</a>
         </article>
     @endforeach
 </div>
