bfbe05514ac421c2a180147a57bb2d5a8ef3921b
asmi046 <asmi046@gmail.com>
2026-09-15 00:23:40 +0300

    Замена фоток

--- resources/views/components/photograph.blade.php
+++ resources/views/components/photograph.blade.php
@@ -1,29 +1,37 @@
         <div class="container">
             <h2 class="section-title">ФОТОАЛЬБОМ</h2>
             <div class="photograph__gallery">
-                <a data-fslightbox="page-photo-galery" href="{{ asset('images/photo1.webp') }}" class="photograph__gallery-photo">
+                <a data-fslightbox="page-photo-galery" href="{{ asset('images/photo1.webp') }}"
+                    class="photograph__gallery-photo">
                     <img src="{{ asset('images/photo1.webp') }}" alt="Фотографии работы ведущего Александра Потапова">
                 </a>
-                <a data-fslightbox="page-photo-galery" href="{{ asset('images/photo2.webp') }}" class="photograph__gallery-photo">
+                <a data-fslightbox="page-photo-galery" href="{{ asset('images/photo2.webp') }}"
+                    class="photograph__gallery-photo">
                     <img src="{{ asset('images/photo2.webp') }}" alt="Фотографии работы ведущего Александра Потапова">
                 </a>
-                <a data-fslightbox="page-photo-galery" href="{{ asset('images/photo3.png') }}" class="photograph__gallery-photo">
+                <a data-fslightbox="page-photo-galery" href="{{ asset('images/photo3.png') }}"
+                    class="photograph__gallery-photo">
                     <img src="{{ asset('images/photo3.png') }}" alt="Фотографии работы ведущего Александра Потапова">
                 </a>
-                <a data-fslightbox="page-photo-galery" href="{{ asset('images/photo4.webp') }}" class="photograph__gallery-photo">
+                <a data-fslightbox="page-photo-galery" href="{{ asset('images/photo4.webp') }}"
+                    class="photograph__gallery-photo">
                     <img src="{{ asset('images/photo4.webp') }}" alt="Фотографии работы ведущего Александра Потапова">
                 </a>
-                <a data-fslightbox="page-photo-galery" href="{{ asset('images/photo5.webp') }}" class="photograph__gallery-photo">
+                <a data-fslightbox="page-photo-galery" href="{{ asset('images/photo5.webp') }}"
+                    class="photograph__gallery-photo">
                     <img src="{{ asset('images/photo5.webp') }}" alt="Фотографии работы ведущего Александра Потапова">
                 </a>
-                <a data-fslightbox="page-photo-galery" href="{{ asset('images/photo6.png') }}" class="photograph__gallery-photo">
+                <a data-fslightbox="page-photo-galery" href="{{ asset('images/photo6.png') }}"
+                    class="photograph__gallery-photo">
                     <img src="{{ asset('images/photo6.png') }}" alt="Фотографии работы ведущего Александра Потапова">
                 </a>
-                <a data-fslightbox="page-photo-galery" href="{{ asset('images/photo7.png') }}" class="photograph__gallery-photo">
-                    <img src="{{ asset('images/photo7.png') }}" alt="Фотографии работы ведущего Александра Потапова">
-                </a>
-                <a data-fslightbox="page-photo-galery" href="{{ asset('images/photo8.webp') }}" class="photograph__gallery-photo">
+                <a data-fslightbox="page-photo-galery" href="{{ asset('images/photo8.webp') }}"
+                    class="photograph__gallery-photo">
                     <img src="{{ asset('images/photo8.webp') }}" alt="Фотографии работы ведущего Александра Потапова">
+                </a>
+                <a data-fslightbox="page-photo-galery" href="{{ asset('images/photo7.webp') }}"
+                    class="photograph__gallery-photo">
+                    <img src="{{ asset('images/photo7.webp') }}" alt="Фотографии работы ведущего Александра Потапова">
                 </a>
             </div>
         </div>
